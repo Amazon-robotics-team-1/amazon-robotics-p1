95d9614dcddb77b9449bba29f5f4ec93e240796d
Shady-Rouk <olaoluwa429@gmail.com>
2024-04-04 07:31:51 -0400

    Change the startup bash script to launch windows instead of tabs, and add a bash script to close the program windows.

--- run_pick_place.sh
+++ run_pick_place.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-gnome-terminal --tab --title="Loading robot in RViz" -- bash -c "source ~/amazon-robotics-p1/install/setup.bash; ros2 launch arm_mobility robot_bringup.launch.py"
-gnome-terminal --tab --title="Loading Pick and Place" -- bash -c "source ~/amazon-robotics-p1/install/setup.bash; ros2 launch arm_mobility robot_pick_place.launch.py"
-gnome-terminal --tab --title="Loading robot camera" -- bash -c "source ~/amazon-robotics-p1/install/setup.bash; ros2 launch kinova_vision kinova_vision.launch.py"
+# Getting the apriltag family passed in while running the script, with default value "tag36h11".
+apriltag_family="${1:-tag36h11}"
+
+gnome-terminal --window --title="Project Shell" -- bash -c "source ~/amazon-robotics-p1/install/setup.bash; ros2 launch arm_mobility robot_bringup.launch.py"
+gnome-terminal --window --title="Project Shell" -- bash -c "source ~/amazon-robotics-p1/install/setup.bash; ros2 launch arm_mobility robot_pick_place.launch.py apriltag_family:=$apriltag_family"
+gnome-terminal --window --title="Project Shell" -- bash -c "source ~/amazon-robotics-p1/install/setup.bash; ros2 launch kinova_vision kinova_vision.launch.py"
