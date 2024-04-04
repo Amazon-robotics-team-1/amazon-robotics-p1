ba67779ccaa472cce2c6cd8602bc3a0757830c38
Kofs7 <wyoofi@gmail.com>
2024-04-04 16:40:24 -0400

    Adding button to close open terminal windows.

--- user_interface/operate.py
+++ user_interface/operate.py
@@ -1,10 +1,11 @@
 # Webpage for the user interface.
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from run_script import run_script
 
 
-SCRIPT_PATH = "/home/senior-project/Desktop/amazon-robotics-p1/run_pick_place.sh"
+RUN_PICK_PLACE_SCRIPT = "/home/senior-project/Desktop/amazon-robotics-p1/run_pick_place.sh"
+CLOSE_PROGRAM_SCRIPT = "/home/senior-project/Desktop/amazon-robotics-p1/close_program.sh"
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,10 +14,15 @@ def index():
 
 @app.route('/script', methods=['POST'])
 def script(result=None):
-    # Execute your Python script here
-    result = run_script(SCRIPT_PATH)
+    # Execute script to open terminal windows
+    result = run_script(script_path=RUN_PICK_PLACE_SCRIPT)
     return render_template('results.html', result=result)
 
+@app.route('/close-route', methods=['POST'])
+def close():
+    # Execute script to close terminal windows
+    run_script(script_path=CLOSE_PROGRAM_SCRIPT)
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True)
